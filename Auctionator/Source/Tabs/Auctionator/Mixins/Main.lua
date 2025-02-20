45e9ae8fc1291b2d9d00ef9f40f85647b555760d
Will <freewill0918@gmail.com>
2025-02-20 21:29:11 +0800

    Update AddOns
    - Auctionator
    - Syndicator

--- Auctionator/Source/Tabs/Auctionator/Mixins/Main.lua
+++ Auctionator/Source/Tabs/Auctionator/Mixins/Main.lua
@@ -3,7 +3,7 @@ AuctionatorConfigTabMixin = {}
 function AuctionatorConfigTabMixin:OnLoad()
   Auctionator.Debug.Message("AuctionatorConfigTabMixin:OnLoad()")
 
-  if Auctionator.Constants.IsClassic then
+  if Auctionator.Constants.IsLegacyAH then
     -- Reposition lower down translator entries so that they don't go past the
     -- bottom of the tab
     self.esES:SetPoint("TOPLEFT", self.deDE, "TOPLEFT", 300, 0)
@@ -18,6 +18,6 @@ function AuctionatorConfigTabMixin:OpenOptions()
     Settings.OpenToCategory(AUCTIONATOR_L_ADDON_OPTIONS)
   else
     InterfaceOptionsFrame:Show()
-    InterfaceOptionsFrame_OpenToCategory(AUCTIONATOR_L_ADDON_OPTIONS)
+    InterfaceOptionsFrame_OpenToCategory(AUCTIONATOR_L_CONFIG_BASIC_OPTIONS_CATEGORY)
   end
 end
